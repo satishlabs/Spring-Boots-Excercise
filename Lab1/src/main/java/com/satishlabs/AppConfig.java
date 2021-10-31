219b823c51e980b96b3871734a0421f66a7be8f9
Satish Prasad <spd61185@gmail.com>
2021-10-31 17:43:54 +0530

    Lab1

---
+++ Lab1/src/main/java/com/satishlabs/AppConfig.java
@@ -0,0 +1,9 @@
+package com.satishlabs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.satishlabs"})
+public class AppConfig {
+}
